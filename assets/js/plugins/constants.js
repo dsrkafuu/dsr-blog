52540ed4fda7aca95807c6aa26456f975e858db2
amzrk2 <amzrk2@outlook.com>
2020-09-04 11:47:21 +0800

    new: feature detection

--- assets/js/plugins/constants.js
+++ assets/js/plugins/constants.js
@@ -1,2 +1,4 @@
 export const DOCUMENT_THEME = 'data-theme';
 export const STORAGE_THEME = 'amzrk2-theme';
+export const NOTIFICATION_FEATURE_SUPPORTED = 'amzrk2-notification-feature-supported';
+export const NOTIFICATION_FEATURE_DISMISSED = 'amzrk2-notification-feature-dismissed';
