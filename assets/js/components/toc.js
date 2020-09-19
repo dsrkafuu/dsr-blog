de2b99df5bb73222c20850ab3f8c9946bb4dd3ba
amzrk2 <amzrk2@outlook.com>
2020-09-19 10:13:01 +0800

    update: reorganizing JavaScript code

---
+++ assets/js/components/toc.js
@@ -0,0 +1,13 @@
+export default function initMobileToc() {
+  const tocNode = document.getElementById('table-of-contents');
+  if (tocNode) {
+    document.getElementById('ctrl-toc').addEventListener('click', () => {
+      const classes = Array.from(tocNode.classList);
+      if (classes.includes('hidden')) {
+        tocNode.classList.remove('hidden');
+      } else {
+        tocNode.classList.add('hidden');
+      }
+    });
+  }
+}
--- assets/js/search.js
+++ assets/js/search.js
@@ -1,6 +1,5 @@
 /* mail */
 import loadEmail from './components/mail';
-
 loadEmail('amzrk2@outlook.com');
 
 /* search engine */
