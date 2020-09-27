1132957fd18bc6a490ed2afd2f9026750a328971
amzrk2 <amzrk2@outlook.com>
2020-09-27 16:03:52 +0800

    new: allow to disable comments

--- assets/js/components/comment.js
+++ assets/js/components/comment.js
@@ -9,18 +9,21 @@ import {
   DISQUS_ADMIN,
 } from '../plugins/constants';
 
-const apikey = [];
-DISQUS_API_KEYS.forEach((val) => {
-  apikey.push(decData(val));
-});
-new DisqusJS({
-  shortname: DISQUS_SHORTNAME,
-  siteName: DISQUS_SITENAME,
-  identifier: document.location.pathname,
-  url: document.location.origin + document.location.pathname + document.location.search,
-  api: DISQUS_API_PROXY,
-  apikey,
-  nocomment: '暂无评论',
-  admin: DISQUS_ADMIN,
-  adminLabel: '管理员',
-});
+const needComment = document.getElementById('disqus_thread');
+if (needComment) {
+  const apikey = [];
+  DISQUS_API_KEYS.forEach((val) => {
+    apikey.push(decData(val));
+  });
+  new DisqusJS({
+    shortname: DISQUS_SHORTNAME,
+    siteName: DISQUS_SITENAME,
+    identifier: document.location.pathname,
+    url: document.location.origin + document.location.pathname + document.location.search,
+    api: DISQUS_API_PROXY,
+    apikey,
+    nocomment: '暂无评论',
+    admin: DISQUS_ADMIN,
+    adminLabel: '管理员',
+  });
+}
