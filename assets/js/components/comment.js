026bff89b08e2581d89dbe58431ffeaecc082f0f
amzrk2 <amzrk2@outlook.com>
2020-09-29 15:30:28 +0800

    fix: try bring disqus back

--- assets/js/components/comment.js
+++ assets/js/components/comment.js
@@ -3,6 +3,9 @@ import { DISQUS_SHORTNAME } from '../plugins/constants';
 
 const needComment = document.getElementById('disqus_thread');
 if (needComment) {
-  const identifier = needComment.getAttribute('data-id') || 'error';
-  const url = window.location.href || 'https://amzrk2.cc/';
+  var d = document,
+    s = d.createElement('script');
+  s.src = `https://${DISQUS_SHORTNAME}.disqus.com/embed.js`;
+  s.setAttribute('data-timestamp', +new Date());
+  (d.head || d.body).appendChild(s);
 }
