d4c1a519b4c9b4424184474828a61cacf3be31f2
amzrk2 <amzrk2@outlook.com>
2020-10-24 19:35:16 +0800

    fix: only load hm in production

--- assets/js/plugins/baiduAnalytics.js
+++ assets/js/plugins/baiduAnalytics.js
@@ -1,5 +1,10 @@
 import { HM_SRC } from './constants';
-const hm = document.createElement('script');
-hm.setAttribute('async', '');
-hm.src = HM_SRC;
-document.head.appendChild(hm);
+import { envIsProd } from './env';
+
+envIsProd() &&
+  (() => {
+    const hm = document.createElement('script');
+    hm.setAttribute('async', '');
+    hm.src = HM_SRC;
+    document.head.appendChild(hm);
+  })();
