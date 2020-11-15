22830e375fd393f03f4a9b31220d0138ad1249d5
amzrk2 <amzrk2@outlook.com>
2020-11-15 16:25:47 +0800

    update: only add license in selection without code

--- assets/js/components/clipboard.js
+++ assets/js/components/clipboard.js
@@ -1,6 +1,28 @@
 import { COPY_LICENSE, BODY_ATTRIBUTE_SECTION, BODY_ATTRIBUTE_HIDE } from '../plugins/constants';
 
 /**
+ * 获取父节点
+ * @param {HTMLElement} node 当前节点
+ * @param {Number} count 递归层数
+ */
+function getAllParentNodes(node, count = 4) {
+  const allParentNodes = [];
+  let recCount = 1; // 递归层数
+  /**
+   * @param {HTMLElement} node
+   */
+  const getParentNodes = (node) => {
+    if (recCount++ >= count || !node.parentNode) {
+      return;
+    }
+    allParentNodes.push(node.parentNode.nodeName);
+    return getParentNodes(node.parentNode);
+  };
+  getParentNodes(node);
+  return allParentNodes;
+}
+
+/**
  * 剪贴板拦截
  */
 if (
@@ -8,12 +30,21 @@ if (
   document.body.getAttribute(BODY_ATTRIBUTE_SECTION) === 'single'
 ) {
   document.addEventListener('copy', (event) => {
-    const clipboardData = event.clipboardData; // 获取剪贴板
-    if (clipboardData) {
-      let copiedText = window.getSelection().toString(); // 获取选择的内容
-      if (copiedText) {
-        event.preventDefault(); // 防止默认行为复制原文内容
-        clipboardData.setData('text/plain', `${copiedText}\n\n${COPY_LICENSE}`);
+    if (event.clipboardData) {
+      const selection = window.getSelection(); // 获取选择的内容
+      // 检查是否代码块
+      const nodes = [
+        getAllParentNodes(selection.anchorNode).join(),
+        getAllParentNodes(selection.focusNode).join(),
+      ].join(' ');
+      // 如果不是代码块
+      if (!/(CODE|PRE).* .*(CODE|PRE)/gi.exec(nodes)) {
+        // 添加 LICENSE
+        let copiedText = selection.toString();
+        if (copiedText) {
+          event.preventDefault(); // 防止默认行为复制原文内容
+          event.clipboardData.setData('text/plain', `${copiedText}\n\n${COPY_LICENSE}`);
+        }
       }
     }
   });
