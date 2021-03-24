07e766bf86c83d24f45c336460ecd154430ab8d0
amzrk2 <amzrk2@outlook.com>
2021-03-24 13:19:47 +0800

    fix: secret envs

--- assets/js/plugins/constants.js
+++ assets/js/plugins/constants.js
@@ -10,8 +10,8 @@ export const SEARCH_API_URL = 'https://workers.dsrkafuu.su/gcse-blog';
 
 // gitalk
 export const GITALK_PROXY = 'https://workers.dsrkafuu.su/gh-oauth';
-export const GITALK_CLIENT_ID = 'ba05c2d3a770e5be487a';
-export const GITALK_CLIENT_SECRET = '0a66856fbcfeaf73cc2a444904d0efa99e208b75';
+export const GITALK_CLIENT_ID = process.env.GITALK_CLIENT_ID;
+export const GITALK_CLIENT_SECRET = process.env.GITALK_CLIENT_SECRET;
 export const GITALK_REPO = 'dsr-blog-comments';
 export const GITALK_OWNER = 'dsrkafuu';
 export const GITALK_ADMIN = ['dsrkafuu', 'dsrsatori'];
@@ -22,3 +22,6 @@ export const COPY_LICENSE = `
 本站内容采用 CC BY-NC-SA 4.0 进行许可；
 商业转载请联系作者授权，非商业转载请注明出处；
 来源：${window.location.href}`;
+
+// sentry
+export const SENTRY_DSN = process.env.SENTRY_DSN;
