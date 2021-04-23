f89aa2c825daf9d09f7138bbed7826467a6401aa
dsrkafuu <dsrkafuu@outlook.com>
2021-04-23 09:24:20 +0800

    refactor: single page style

---
+++ assets/js/list.js
@@ -0,0 +1,26 @@
+/*! dsr-blog | DSRKafuU (https://dsrkafuu.su) | Copyright (c) Apache-2.0 License */
+
+import * as Sentry from '@sentry/browser';
+import { Integrations } from '@sentry/tracing';
+import { SENTRY_DSN } from './plugins/constants';
+
+// sentry
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
+}
+
+// iconfont
+import iconfont from '../svg/iconfont';
+iconfont(window);
+
+// theme system
+import ThemeManager from './components/theme';
+const tm = new ThemeManager();
+const switcher = document.querySelector('#ctrl-adjust');
+if (switcher) {
+  switcher.addEventListener('click', () => tm.switchTheme());
+}
