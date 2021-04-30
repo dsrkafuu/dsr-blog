bab45e30c9aba3b9cfcb1b7adffe62645613a9a5
dsrkafuu <dsrkafuu@outlook.com>
2021-04-30 10:40:49 +0800

    fix: no toc needed page's toc control

--- assets/js/components/toc.js
+++ assets/js/components/toc.js
@@ -6,8 +6,14 @@ import { ID_TOC_CONTENT, ID_TOC_CTRL } from '../plugins/constants';
 export default function () {
   const tocContent = document.querySelector(`#${ID_TOC_CONTENT}`);
   const tocCtrl = document.querySelector(`#${ID_TOC_CTRL}`);
-  if (!tocContent || !tocCtrl) {
+  if (!tocContent && tocCtrl) {
+    logInfo('no need to init toc');
+    tocCtrl.remove();
+    return;
+  }
+  if (!tocCtrl) {
     logError('toc related element not found');
+    return;
   }
   tocCtrl.addEventListener('click', () => {
     if (Array.from(tocContent.classList).includes('active')) {
