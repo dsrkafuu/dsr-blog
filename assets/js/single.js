827bb1fe967b65a33047cbd98d27f6f9b37b177a
dsrkafuu <dsrkafuu@outlook.com>
2021-05-09 21:05:25 +0800

    refactor: load prism inside script

--- assets/js/single.js
+++ assets/js/single.js
@@ -13,3 +13,22 @@ if (process.env.NODE_ENV === 'production') {
 // gitalk comment
 import comment from './components/comment';
 comment();
+
+// prismjs
+import { loadScript } from './plugins/loaders';
+import { SCRIPT_PRISM_LOADER, PRISM_LANGS_PATH } from './plugins/constants';
+import { logError } from './plugins/loggers';
+if (document.querySelector('pre code')) {
+  // set to manual mode
+  window.Prism = window.Prism || {};
+  window.Prism.manual = true;
+  // loading
+  loadScript(SCRIPT_PRISM_LOADER)
+    .then(() => {
+      window.Prism.plugins.autoloader.languages_path = PRISM_LANGS_PATH;
+      window.Prism.highlightAll();
+    })
+    .catch((e) => {
+      logError('error loading prism', e);
+    });
+}
