9ce52ab4a33aff671acfc885773616b4e0a7cc41
dsrkafuu <dsrkafuu@outlook.com>
2022-06-27 13:34:54 +0800

    fix: back to jsdelivr

--- assets/js/components/twemoji.js
+++ assets/js/components/twemoji.js
@@ -4,6 +4,10 @@
 export default () => {
   const content = document.querySelector('div.content');
   if (content && window.twemoji) {
-    window.twemoji.parse(content, { folder: 'svg', ext: '.svg' });
+    window.twemoji.parse(content, {
+      base: 'https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/',
+      folder: 'svg',
+      ext: '.svg',
+    });
   }
 };
