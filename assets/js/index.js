0c0074d3ab2753e69c9be0371a25fc0994cf69cd
dsrkafuu <dsrkafuu@outlook.com>
2022-11-25 21:14:00 +0800

    fix: move to new design

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,4 @@
-import * as Sentry from '@sentry/browser';
-import { BrowserTracing } from '@sentry/tracing';
-import { logError, logInfo } from './plugins/loggers';
+import { logInfo } from './plugins/loggers';
 import { SECTIONS, getSection } from './plugins/utils';
 import search from './components/search';
 import toc from './components/toc';
@@ -9,22 +7,6 @@ import zoom from './components/zoom';
 
 const SECTION = getSection();
 
-// sentry
-if (process.env.NODE_ENV === 'production') {
-  try {
-    Sentry.init({
-      dsn: process.env.HUGO_SENTRY_DSN,
-      integrations: [new BrowserTracing()],
-      sampleRate: 1, // report all errors
-      tracesSampleRate: 0.05, // report 5% of traces
-    });
-    logInfo('sentry module inited');
-  } catch (e) {
-    logError('error init sentry', e);
-  }
-}
-
-// modules
 twemoji();
 logInfo('twemoji module inited');
 if ([SECTIONS.INDEX, SECTIONS.LIST].includes(SECTION)) {
